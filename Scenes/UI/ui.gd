32b58770eefaf261aa67c2fa855e86355b07a263
kkuratolodon <mif05123@gmail.com>
2024-09-26 09:04:43 +0700

    nambahin store sementara

--- Scenes/UI/ui.gd
+++ Scenes/UI/ui.gd
@@ -1,18 +1,21 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
 	pass
 
-
 func _on_settings_button_pressed() -> void:
-	var settings_scene = preload("res://Scenes/UI/Settings/settings.tscn")
-	var instance = settings_scene.instantiate()
-	add_child(instance)
-	
+	var settings_scene = preload("res://Scenes/UI/Settings/settings.tscn").instantiate()
+	add_child(settings_scene)
+
+func _on_coin_store_button_pressed() -> void:
+	var store_scene = preload("res://Scenes/UI/Store/store.tscn").instantiate()
+	var scroll_container = store_scene.get_node("ScrollContainer")
+	add_child(store_scene)
+	scroll_container.scroll_vertical = 0
+
+func _on_gem_store_button_pressed() -> void:
+	var store_scene = preload("res://Scenes/UI/Store/store.tscn").instantiate()
+	var scroll_container = store_scene.get_node("ScrollContainer")
+	add_child(store_scene)
+	scroll_container.scroll_vertical = scroll_container.get_v_scroll_bar().max_value
